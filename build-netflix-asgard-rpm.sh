64c40f4bd01bc9a0d587a873be5bace68e89be39
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2014-08-01 23:36:03 +0200

    Update build-netflix-asgard-rpm.sh

--- build-netflix-asgard-rpm.sh
+++ build-netflix-asgard-rpm.sh
@@ -4,4 +4,4 @@ VERSION=1.5
 
 rm -f asgard.war
 wget https://github.com/Netflix/asgard/releases/download/asgard-$VERSION/asgard.war
-fpm -s dir -t rpm -n asgard -v $VERSION -d tomcat7 --rpm-os linux --url https://github.com/Netflix/asgard --prefix /usr/share/tomcat7/webapps asgard.war
+fpm -s dir -t rpm -n asgard -v $VERSION -d tomcat7 -d tomcat-native --rpm-os linux --url https://github.com/Netflix/asgard --prefix /usr/share/tomcat7/webapps asgard.war
