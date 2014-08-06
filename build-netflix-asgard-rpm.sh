ca730532604e77b54e44e72ecf560df037629586
Marco Hoyer <marco@hoyer.pm>
2014-08-06 17:40:33 +0200

    add release var and deploy asgard in root-context

--- build-netflix-asgard-rpm.sh
+++ build-netflix-asgard-rpm.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 VERSION=1.5
+RELEASE=2
 
 rm -f asgard.war
 wget https://github.com/Netflix/asgard/releases/download/asgard-$VERSION/asgard.war
-fpm -s dir -t rpm -n asgard -v $VERSION -d tomcat7 -d tomcat-native --rpm-os linux --url https://github.com/Netflix/asgard --prefix /usr/share/tomcat7/webapps asgard.war
+mv asgard.war ROOT.war
+fpm -s dir -t rpm -n asgard -v $VERSION --iteration $RELEASE -d tomcat7 -d tomcat-native --rpm-os linux --url https://github.com/Netflix/asgard --prefix /usr/share/tomcat7/webapps ROOT.war
+rm ROOT.war
